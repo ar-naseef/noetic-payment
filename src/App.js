b691976ba77d79a5669798a5f9ef34b91419e754
naseefxyz <naseef@appler.xyz>
2018-11-19 22:44:52 +0530

    changes in app level

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import './App.css';
+import store from './store';
+import 'bulma/css/bulma.css';
+
+import Modal from './components/Modal';
+import Mainpage from './components/MainPage';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalActive: false
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>Hello <code>React and Redux</code></h1>
-      </div>
+      <Provider store={ store }>
+        <div className="App">
+          <Modal />
+          <Mainpage />
+        </div>
+      </Provider>
     );
   }
 }
