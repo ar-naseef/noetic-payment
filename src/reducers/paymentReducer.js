d47e62f6879d620e054ed0fde1c8d19becbc0e7f
naseefxyz <naseef@appler.xyz>
2018-11-19 22:47:18 +0530

    Changes in reducers

---
+++ src/reducers/paymentReducer.js
@@ -0,0 +1,34 @@
+import {
+  UPDATE_INPUT,
+  SHOW_MODAL,
+  VALIDATE_CARD
+} from '../actions/types';
+
+const initialState = {
+  showModal: true,
+  inputObj: {},
+  cardData: {}
+};
+
+
+export default function (state = initialState, action) {
+  const {
+    inputObj,
+    show,
+    cardData
+  } = action;
+
+  switch (action.type) {
+    case UPDATE_INPUT:
+      return Object.assign({}, state, { inputObj });
+
+    case SHOW_MODAL:
+      return Object.assign({}, state, { showModal: show });
+
+    case VALIDATE_CARD:
+      return Object.assign({}, state, { cardData });
+
+    default:
+      return state;
+  }
+}
